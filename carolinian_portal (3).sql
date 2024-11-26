edc1cd55a1b30f67c2b970c6e920245a9a773742
unclejambo <134165062+unclejambo@users.noreply.github.com>
2024-11-27 02:28:01 +0800

    Add files via upload

---
+++ carolinian_portal (3).sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2024 at 07:26 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `carolinian_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+-- Error reading structure for table carolinian_portal.courses: #1932 - Table &#039;carolinian_portal.courses&#039; doesn&#039;t exist in engine
+-- Error reading data for table carolinian_portal.courses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `carolinian_portal`.`courses`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+-- Error reading structure for table carolinian_portal.documents: #1932 - Table &#039;carolinian_portal.documents&#039; doesn&#039;t exist in engine
+-- Error reading data for table carolinian_portal.documents: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `carolinian_portal`.`documents`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+-- Error reading structure for table carolinian_portal.students: #1932 - Table &#039;carolinian_portal.students&#039; doesn&#039;t exist in engine
+-- Error reading data for table carolinian_portal.students: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `carolinian_portal`.`students`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
